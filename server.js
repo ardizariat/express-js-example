f31bf6c4f2aad3e8eaab4c59e62791c1a420723b
Ardi Nor Dzariat <55976230+ardizariat@users.noreply.github.com>
2024-09-06 15:05:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).json({
+    message : "success"
+  })
 });
 
 app.listen(port, () => {
